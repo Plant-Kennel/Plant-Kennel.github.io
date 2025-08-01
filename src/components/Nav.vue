efd41307cf8fda40bf94444c232c4f7a15eac417
elbi3 <ellbi3@fastmail.com>
2025-07-31 19:19:13 -0700

    Add Nav component and fix up base and main css files

---
+++ src/components/Nav.vue
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<template>
+    <nav>
+        <a href="home">Home</a>
+        <a href="about">About</a>
+    </nav>
+</template>
+
+<style>
+    nav {
+        background-color: aliceblue;
+        list-style: none;
+        display: flex;
+    }
+
+    a {
+        color: green;
+        text-decoration: none;
+    }
+</style>
